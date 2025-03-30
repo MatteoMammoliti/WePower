53ed0310d7071e9fb8154f6ca434f55b3c42192d
Matteo Mammoliti <matti.mm04@gmail.com>
2025-03-30 15:51:55 +0200

    Primi codici in LoginController

--- src/main/java/com/wepower/wepower/Controllers/LoginController.java
+++ src/main/java/com/wepower/wepower/Controllers/LoginController.java
@@ -1,4 +1,12 @@
 package com.wepower.wepower.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public TextField username_text_field;
+    public PasswordField password_field;
+    public Button accedi_button;
+    public Button signup_button;
 }
--- src/main/java/com/wepower/wepower/WePower.java
+++ src/main/java/com/wepower/wepower/WePower.java
@@ -1,5 +1,19 @@
 package com.wepower.wepower;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class WePower {
+public class WePower extends Application
+{
 
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // creaimo un oggetto fxml loader per caricare la scena del login in memoria
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
