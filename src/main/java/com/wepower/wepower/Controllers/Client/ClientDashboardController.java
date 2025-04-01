6a23ce94ffde2054dde2f76fe597226a49f8641d
loriacristian <lrocst03s28h919w@studenti.unical.it>
2025-04-01 20:25:19 +0200

    SCHIZZO Dashboard

---
+++ src/main/java/com/wepower/wepower/Controllers/Client/ClientDashboardController.java
@@ -0,0 +1,11 @@
+package com.wepower.wepower.Controllers.Client;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+
+public class ClientDashboardController {
+
+
+}
