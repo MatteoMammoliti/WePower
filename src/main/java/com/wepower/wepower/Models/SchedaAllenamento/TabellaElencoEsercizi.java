1a121da12bc56237a3d9e64795ba36c2e4f85753
Matteo Mammoliti <matti.mm04@gmail.com>
2025-04-11 19:55:53 +0200

    Implementazione massimali

--- src/main/java/com/wepower/wepower/Models/SchedaAllenamento/TabellaElencoEsercizi.java
+++ src/main/java/com/wepower/wepower/Models/SchedaAllenamento/TabellaElencoEsercizi.java
@@ -46,11 +46,20 @@ public class TabellaElencoEsercizi {
         ArrayList<RigaEsercizioScheda> ris = new ArrayList<RigaEsercizioScheda>();
 
         String query = "SELECT e.NomeEsercizio, e.DescrizioneEsercizio, e.MuscoloAllenato, e.PercorsoImmagine, " +
-                "csa.NumeroRipetizioni, csa.NumeroSerie " +
+                "csa.NumeroRipetizioni, csa.NumeroSerie, m.Peso " +
                 "FROM SchedaAllenamento s " +
                 "JOIN ComposizioneSchedaAllenamento csa ON s.IdScheda = csa.IdSchedaAllenamento " +
                 "JOIN Esercizio e ON csa.NomeEsercizio = e.NomeEsercizio " +
-                "WHERE s.idCliente = ?";
+                "LEFT JOIN ( " +
+                "   SELECT mic.NomeEsercizio, mic.IdCliente, mic.Peso " +
+                "   FROM MassimaleImpostatoCliente mic " +
+                "   INNER JOIN ( " +
+                "       SELECT NomeEsercizio, IdCliente, MAX(DataInserimento) AS MaxData " +
+                "       FROM MassimaleImpostatoCliente " +
+                "       GROUP BY NomeEsercizio, IdCliente " +
+                "   ) latest ON mic.NomeEsercizio = latest.NomeEsercizio AND mic.IdCliente = latest.IdCliente AND mic.DataInserimento = latest.MaxData " +
+                ") m ON csa.NomeEsercizio = m.NomeEsercizio AND m.IdCliente = s.IdCliente " +
+                "WHERE s.IdCliente = ?";
 
         try (Connection conn = ConnessioneDatabase.getConnection()) {
             PreparedStatement datiEsercizi = conn.prepareStatement(query);
@@ -63,12 +72,19 @@ public class TabellaElencoEsercizi {
                 String MuscoloAllenato = risultatoTuttiEsercizi.getString("MuscoloAllenato");
                 String NumeroSerie = risultatoTuttiEsercizi.getString("NumeroSerie");
                 String NumeroRipetizioni = risultatoTuttiEsercizi.getString("NumeroRipetizioni");
+
+                String massimaleAttuale = risultatoTuttiEsercizi.getString("Peso");
+
+                if (massimaleAttuale == null) {
+                    massimaleAttuale = "0";
+                }
+
                 String PercorsoImmagine = risultatoTuttiEsercizi.getString("PercorsoImmagine");
 
                 if (PercorsoImmagine == null || PercorsoImmagine.trim().isEmpty()) {
                     PercorsoImmagine = "images/LOGO.png";
                 }
-                RigaEsercizioScheda esercizio = new RigaEsercizioScheda(NomeEsercizio, DescrizioneEsercizio, MuscoloAllenato, NumeroSerie, NumeroRipetizioni, PercorsoImmagine, aggiornaUI);
+                RigaEsercizioScheda esercizio = new RigaEsercizioScheda(NomeEsercizio, DescrizioneEsercizio, MuscoloAllenato, NumeroSerie, NumeroRipetizioni, PercorsoImmagine, massimaleAttuale, aggiornaUI);
                 ris.add(esercizio);
             }
         } catch (Exception e) {
--- src/main/java/com/wepower/wepower/Views/SchedaAllenamento/RigaEsercizioScheda.java
+++ src/main/java/com/wepower/wepower/Views/SchedaAllenamento/RigaEsercizioScheda.java
@@ -5,6 +5,7 @@ import com.wepower.wepower.Models.DatiSessioneCliente;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -20,23 +21,39 @@ public class RigaEsercizioScheda extends HBox {
     private Label numeroSerie;
     private Label numeroRipetizioni;
     private ImageView imageEsercizio;
+    private Label massimaleAttuale;
+    private TextField aggiungiNuovoMassimale;
+    private Button aggiungiMassimale;
     private Button rimuoviSchedaEsercizio;
 
 
-    public RigaEsercizioScheda(String nomeEsercizio, String descrizioneEsercizio, String muscoloAllenato, String numeroSerie, String numeroRipetizioni, String percorsoImmagine, Runnable aggiornaUI) {
+    public RigaEsercizioScheda(String nomeEsercizio, String descrizioneEsercizio, String muscoloAllenato, String numeroSerie, String numeroRipetizioni, String percorsoImmagine, String massimaleAttuale, Runnable aggiornaUI) {
         this.nomeEsercizio = new Label(nomeEsercizio);
         this.descrizioneEsercizio = new Label(descrizioneEsercizio);
         this.muscoloAllenato = new Label("Muscolo allenato: " + muscoloAllenato);
         this.numeroSerie = new Label(numeroSerie);
         this.numeroRipetizioni = new Label(numeroRipetizioni);
 
-        InputStream is = getClass().getResourceAsStream("/" + percorsoImmagine);
+        this.massimaleAttuale = new Label("Massimale attuale: " + massimaleAttuale);
+        // eventualmente inserire data
 
+        this.aggiungiNuovoMassimale = new TextField();
+        this.aggiungiNuovoMassimale.setPromptText("Aggiungi nuovo massimale");
+        this.aggiungiNuovoMassimale.setVisible(false);
+
+
+        InputStream is = getClass().getResourceAsStream("/" + percorsoImmagine);
         Image image = new Image(is);
         this.imageEsercizio = new ImageView(image);
         this.imageEsercizio.setFitHeight(100);
         this.imageEsercizio.setFitWidth(100);
         this.imageEsercizio.setPreserveRatio(true);
+
+        this.aggiungiMassimale = new Button("Aggiungi massimale");
+        this.aggiungiMassimale.setOnAction(event -> {
+            onAggiungiNuovoMassimale();
+            aggiornaUI.run();
+        });
 
         this.rimuoviSchedaEsercizio = new Button("Rimuovi esercizio dalla scheda");
         this.rimuoviSchedaEsercizio.setOnAction(event -> {
@@ -48,19 +65,42 @@ public class RigaEsercizioScheda extends HBox {
             }
         });
 
-        this.getChildren().addAll(this.nomeEsercizio, this.descrizioneEsercizio, this.muscoloAllenato, this.numeroSerie, this.numeroRipetizioni, this.imageEsercizio, this.rimuoviSchedaEsercizio);
+        this.getChildren().addAll(this.nomeEsercizio, this.descrizioneEsercizio, this.muscoloAllenato, this.numeroSerie, this.numeroRipetizioni, this.imageEsercizio, this.massimaleAttuale, this.aggiungiNuovoMassimale,
+                this.aggiungiMassimale,this.rimuoviSchedaEsercizio);
         this.setSpacing(10);
         this.setPadding(new Insets(10));
     }
 
     private void onRimuoviEsercizio() throws SQLException {
         String eliminaEsercizio = "DELETE FROM ComposizioneSchedaAllenamento WHERE NomeEsercizio = ? AND IdSchedaAllenamento = ?";
+        String eliminaMassimale = "DELETE FROM MassimaleImpostatoCliente WHERE NomeEsercizio = ? AND IdCliente = ?";
 
         try (Connection conn = ConnessioneDatabase.getConnection()) {
             PreparedStatement eliminazione = conn.prepareStatement(eliminaEsercizio);
             eliminazione.setString(1, this.nomeEsercizio.getText());
             eliminazione.setInt(2, DatiSessioneCliente.getIdSchedaAllenamento());
             eliminazione.executeUpdate();
+
+            PreparedStatement eliminazioneMassimale = conn.prepareStatement(eliminaMassimale);
+            eliminazioneMassimale.setString(1, this.nomeEsercizio.getText());
+            eliminazioneMassimale.setInt(2, DatiSessioneCliente.getIdUtente());
+            eliminazioneMassimale.executeUpdate();
+        }
+    }
+
+    private void onAggiungiNuovoMassimale () {
+        this.aggiungiNuovoMassimale.setVisible(true);
+        String massimale = "INSERT INTO MassimaleImpostatoCliente (IdCliente, NomeEsercizio, DataInserimento, Peso) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = ConnessioneDatabase.getConnection()) {
+            PreparedStatement inserimento = conn.prepareStatement(massimale);
+            inserimento.setInt(1, DatiSessioneCliente.getIdUtente());
+            inserimento.setString(2, this.nomeEsercizio.getText());
+            inserimento.setDate(3, java.sql.Date.valueOf(java.time.LocalDate.now()));
+            inserimento.setDouble(4, Double.parseDouble(this.aggiungiNuovoMassimale.getText()));
+            inserimento.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
